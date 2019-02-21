ef87b25aa212f0262e748a5dace878a564e29319
trapajim <mail@tk-dev.de>
2019-02-21 17:06:22 +0900

    added game timer and start screen

--- minigames/letter.go
+++ minigames/letter.go
@@ -9,13 +9,19 @@ import (
 )
 
 const (
-	Refresh      = 60 * time.Millisecond
-	Color        = termbox.ColorCyan
-	BorderColor  = termbox.ColorWhite
-	DefaultColor = termbox.ColorDefault
+	Refresh                = 60 * time.Millisecond
+	Color                  = termbox.ColorCyan
+	BorderColor            = termbox.ColorWhite
+	DefaultColor           = termbox.ColorDefault
+	GameTime               = 15
+	Menu         GameState = iota
+	Playing
+	Quit
 )
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyz1234567890")
+
+type GameState int
 
 type Vector2d struct {
 	x, y int
@@ -32,10 +38,12 @@ type GameElement struct {
 }
 
 type Context struct {
-	gameEl  GameElement
-	counter int
-	world   World
-	quit    bool
+	gameEl    GameElement
+	world     World
+	counter   int
+	time      int
+	ticker    *time.Ticker
+	gameState GameState
 }
 
 func (v *GameElement) Draw(left, bottom int) {
@@ -60,16 +68,17 @@ func (ctx *Context) Draw() {
 	termbox.Clear(DefaultColor, DefaultColor)
 	world := ctx.world
 	var (
-		w, h = termbox.Size()
-		midY = h / 2
-		left = (w - world.width) / 2
-		//right  = (w + world.width) / 2
+		w, h   = termbox.Size()
+		midY   = h / 2
+		left   = (w - world.width) / 2
+		right  = (w + world.width) / 2
 		top    = midY - (world.height / 2)
 		bottom = midY + (world.height / 2) + 1
 	)
 	ctx.world.Draw(top, bottom, left)
 	ctx.gameEl.Draw(left, bottom)
 	tbprint(left, bottom, DefaultColor, DefaultColor, strconv.Itoa(ctx.counter))
+	tbprint(right, bottom, DefaultColor, DefaultColor, strconv.Itoa(ctx.time))
 	termbox.Flush()
 }
 func fill(x, y, w, h int, cell termbox.Cell) {
@@ -90,13 +99,14 @@ func NewGameElement(x, y int, letter rune) GameElement {
 func NewContext() *Context {
 	world := World{60, 30}
 	return &Context{
-		world:   world,
-		counter: 0,
+		world:     world,
+		counter:   0,
+		gameState: Menu,
+		time:      GameTime,
 	}
 }
 
 func update(ctx *Context, events chan termbox.Event) {
-
 	select {
 	case ev := <-events:
 		if ev.Type == termbox.EventKey {
@@ -105,7 +115,7 @@ func update(ctx *Context, events chan termbox.Event) {
 				ctx.addgameEl()
 				ctx.counter++
 			case ev.Key == termbox.KeyEsc:
-				ctx.quit = true
+				ctx.gameState = Quit
 			case ev.Ch != ctx.gameEl.letter:
 				ctx.addgameEl()
 				ctx.counter--
@@ -113,6 +123,49 @@ func update(ctx *Context, events chan termbox.Event) {
 		}
 	default:
 		ctx.Draw()
+		time.Sleep(Refresh)
+	}
+}
+
+func (ctx *Context) startTimer() {
+	ctx.time = GameTime
+	ctx.ticker = time.NewTicker(1 * time.Second)
+	go func() {
+		for range ctx.ticker.C {
+			ctx.time--
+			if ctx.time <= 0 {
+				ctx.ticker.Stop()
+				ctx.gameState = Menu
+			}
+		}
+	}()
+}
+
+func drawMenu(ctx *Context, events chan termbox.Event) {
+
+	select {
+	case ev := <-events:
+		if ev.Type == termbox.EventKey {
+			switch {
+			case ev.Key == termbox.KeyEsc:
+				ctx.gameState = Quit
+			default:
+				ctx.gameState = Playing
+				ctx.startTimer()
+				ctx.addgameEl()
+			}
+		}
+	default:
+		world := ctx.world
+		var (
+			w, h   = termbox.Size()
+			midY   = h / 2
+			left   = (w - world.width) / 2
+			bottom = midY + (world.height / 2) + 1
+		)
+		termbox.Clear(DefaultColor, DefaultColor)
+		tbprint(left, bottom, DefaultColor, DefaultColor, "Press any button to start")
+		termbox.Flush()
 		time.Sleep(Refresh)
 	}
 }
@@ -147,8 +200,12 @@ func main() {
 
 	ctx := NewContext()
 	rand.Seed(time.Now().Unix())
-	for !ctx.quit {
-		update(ctx, events)
+	for ctx.gameState != Quit {
+		if ctx.gameState == Playing {
+			update(ctx, events)
+		} else {
+			drawMenu(ctx, events)
+		}
 	}
 
 }
--- minigames/readme.md
+++ minigames/readme.md
@@ -2,10 +2,10 @@ letter typing game done with [termbox](https://github.com/nsf/termbox-go)
 
 A random letter will appear and you have to typ it as fast as possible.
 
+Press esc to exit the game.
+
 ---
 
 todo
 
-- add game start
 - add points instad of the counter
-- limit the game time
